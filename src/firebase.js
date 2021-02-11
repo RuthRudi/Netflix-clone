98e8f8021ae8e6dc9721dde60d87dfc14e275a25
Rudi781 <ruth.gideon@aun.edu.ng>
2021-02-11 14:52:08 +0100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBSjtN1AX1hOteiEtnZyR2jNGj3wF7rkcg",
+    authDomain: "netflix-clone-5b783.firebaseapp.com",
+    projectId: "netflix-clone-5b783",
+    storageBucket: "netflix-clone-5b783.appspot.com",
+    messagingSenderId: "116121588964",
+    appId: "1:116121588964:web:e6aef490414d1ef4e088f1"
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
